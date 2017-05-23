76e87974542c89e1b408fc58eb03d5ffd4819b3c
Shaun Djuhari <shaundjuhari@berkeley.edu>
2017-05-23 17:37:30 +0800

    workign script

---
+++ bucketsgsw.py
@@ -0,0 +1,88 @@
+import csv
+
+class player(object):
+    def __init__(self,firstName,lastName, teamName,teamAbbrev,playerNo,colorways):
+        self.firstName=firstName
+        self.lastName=lastName
+        self.teamName=teamName
+        self.teamAbbrev=teamAbbrev
+        self.playerNo=playerNo
+        self.colorways=colorways
+
+sc30=player("Stephen", "Curry", "Golden State Warriors", "gsw", 30,["White Home","Blue Away","Christmas '16","CNY Black"])
+kt11=player("Klay", "Thompson", "Golden State Warriors", "gsw", 11,["White Home","Blue Away","Christmas '16","CNY Black"])
+dg23=player("Draymond", "Green", "Golden State Warriors", "gsw", 23,["White Home","Blue Away","Christmas '16","CNY Black"])
+kd35=player("Kevin", "Durant", "Golden State Warriors", "gsw", 35,["White Home","Blue Away","Christmas '16","CNY Black"])
+ai09=player("Andre", "Iguodala", "Golden State Warriors", "gsw", 9,["White Home","Blue Away","Christmas '16","CNY Black"])
+
+
+with open("gsw.csv", "wb") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+
+        headers =["Handle", "Title","Body (HTML)","Vendor","Type","Tags","Published", "Option1 Name", "Option1 Value", "Option2 Name","Option2 Value","Option3 Name", "Option3 Value","Variant SKU","Variant Grams","Variant Inventory Tracker","Variant Inventory Qty", "Variant Inventory Policy","Variant Fulfillment Service", "Variant Price",
+        "Variant Compare At Price","Variant Requires Shipping","Variant Taxable","Variant Barcode","Image Src","Image Position","Image Alt Text","Gift Card","SEO Title","SEO Description","Google Shopping / Google Product Category","Google Shopping / Gender", "Google Shopping / Age Group","Google Shopping / MPN","Google Shopping / AdWords Grouping","Google Shopping / AdWords Labels",
+        "Google Shopping / Condition","Google Shopping / Custom Product","Google Shopping / Custom Label 0","Google Shopping / Custom Label 1","Google Shopping / Custom Label 2","Google Shopping / Custom Label 3","Google Shopping / Custom Label 4","Variant Image","Variant Weight Unit","Variant Tax Code"]
+        print len(headers)
+        print sc30.firstName
+        writer.writerow(headers)
+        sizes=["S","M","L","XL","XXL"]
+        players = [sc30,kt11,dg23,kd35,ai09]
+        playerstack=[]
+        for player in players:
+            playerstack.append(player)
+
+        for playerhandle in players:
+            
+            colorwaystack=[]
+            for colorway in playerhandle.colorways:
+                colorwaystack.append(colorway)
+            toWrite=[None]*100
+            for colorwayhandle in playerhandle.colorways:
+                first=True;
+                for size in sizes:
+
+                    for colorway in colorwaystack:
+
+                        for player in playerstack:
+                            toWrite=[None]*100
+                            if first==True:
+
+                                title = playerhandle.teamName + " #"+str(playerhandle.playerNo)+" "+playerhandle.firstName+" "+playerhandle.lastName + " NBA Swingman Jersey"
+                                toWrite[1]=title #title
+                                toWrite[3]="Buckets.SG" #vendor
+                                toWrite[4]="Jersey" #type
+                                toWrite[7]="Size" #option1
+                                toWrite[9]="Colorway" #option2
+                                toWrite[11]= "Player" #option3
+                            #loop over colorway and size
+
+                            handle= playerhandle.teamAbbrev + "-" +playerhandle.firstName+"-"+ playerhandle.lastName+"-"+ colorwayhandle.split(' ', 1)[0]
+                            toWrite[0]=handle.lower() #handle
+
+
+
+                            toWrite[8]=size #option1value
+
+                            toWrite[10]= colorway #option2value
+
+                            toWrite[12]= player.firstName +" "+player.lastName+" #"+str(player.playerNo) #option3value
+                            if player.playerNo >=10:
+                                toWrite[13]= (player.teamAbbrev+colorway[0]+colorway[1]+colorway[2]+str(player.playerNo)+size).upper() #sku
+                            else:
+                                toWrite[13]= (player.teamAbbrev+colorway[0]+colorway[1]+colorway[2]+"0"+str(player.playerNo)+size).upper() #sku
+                            toWrite[14]="250"
+                            toWrite[15]="shopify"
+                            toWrite[16]="1"
+                            toWrite[17]="continue"
+                            toWrite[18]="manual"
+                            toWrite[19]="30"
+                            toWrite[21]="TRUE"
+                            toWrite[22]="TRUE"
+                            toWrite[44]="kg"
+                            writer.writerow(toWrite)
+                            first=False;
+                first=True;
+                colorwaystack.pop(0)
+                colorwaystack.append(colorwayhandle)
+            playerstack.pop(0)
+            playerstack.append(playerhandle)
